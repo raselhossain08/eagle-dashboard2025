e95c7b378ef28d3cd9cae02f7cce0dd2ce8fd8e5
raselhossain08 <raselhossain08>
2025-11-10 02:58:28 +0600

    lasta

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://eagleinvest.us/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
